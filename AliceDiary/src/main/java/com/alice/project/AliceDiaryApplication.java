a63f8267e479847cbadb46a0900e69a4f077a818
Yejin-Ha <hyj1004mu@naver.com>
2022-07-08 18:56:16 +0900

    create project

---
+++ AliceDiary/src/main/java/com/alice/project/AliceDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.alice.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceDiaryApplication.class, args);
+	}
+
+}
---
+++ AliceDiary/src/test/java/com/alice/project/AliceDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alice.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
