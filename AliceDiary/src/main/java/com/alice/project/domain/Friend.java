ae696409b4d78116230a9fd7dc4b94acfdf79827
melanieing <melanie0617@gmail.com>
2022-07-23 12:45:05 +0900

    4트

--- AliceDiary/src/main/java/com/alice/project/domain/Friend.java
+++ AliceDiary/src/main/java/com/alice/project/domain/Friend.java
@@ -26,44 +26,43 @@ import lombok.ToString;
 @EqualsAndHashCode(of = "num")
 public class Friend {
 
-   @Id
-   @GeneratedValue
-   @Column(name = "friend_num")
-   private Long num; // 친구 번호
+	@Id
+	@GeneratedValue
+	@Column(name = "friend_num")
+	private Long num; // 친구 번호
 //   private Long adderNum; // 등록하는 친구 회원번호
-   @Column(unique = true)
-   private Long addeeNum; // 등록되는 친구 회원번호
-   private Long groupNum; // 등록된 그룹 번호 (기본그룹 = 1)
+	@Column(unique = true)
+	private Long addeeNum; // 등록되는 친구 회원번호
+	private Long groupNum; // 등록된 그룹 번호 (기본그룹 = 1)
 
 //   @ManyToOne(fetch=FetchType.LAZY)
 //   @JoinColumn(name="group_num")
 //   private FriendsGroup group; // 친구 소속 그룹
 //    
-   @ManyToOne(fetch = FetchType.LAZY) // 모든 연관관계는 항상 지연로딩으로 설정(성능상이점)
-   @JoinColumn(name = "adder_num", unique = true)
-   @JsonBackReference
-   private Member member; // 친구 등록하는 회원 객체
-   
+	@ManyToOne(fetch = FetchType.LAZY) // 모든 연관관계는 항상 지연로딩으로 설정(성능상이점)
+	@JoinColumn(name = "adder_num", unique = true)
+	@JsonBackReference
+	private Member member; // 친구 등록하는 회원 객체
 
-   // 연관관계 메서드 (양방향관계)
+	// 연관관계 메서드 (양방향관계)
 //   public void setGroup(FriendsGroup group) {
 //      this.group = group;
 //      group.getFriends().add(this);
 //   }
-   public void setMember(Member member) {
-      this.member = member;
+	public void setMember(Member member) {
+		this.member = member;
 //      return member.getNum();
 //      member.getFriends().add(this);
-   }
+	}
 
-   @Builder
-   public Friend(Member member, Long addeeNum, Long groupNum) {
+	@Builder
+	public Friend(Member member, Long addeeNum, Long groupNum) {
 //      this.adderNum = setMember(member);
-      setMember(member);
-      this.addeeNum = addeeNum;
-      this.groupNum = groupNum;
-   }
-   // 친구 객체 생성 메서드
+		setMember(member);
+		this.addeeNum = addeeNum;
+		this.groupNum = groupNum;
+	}
+	// 친구 객체 생성 메서드
 //   public static Friend createFriend(FriendsGroup group, Member member) {
 //      Friend friend = new Friend();
 //      friend.setGroup(group);
